d938d5cb4e0b54cefb3dcff78b0981c7c3249f3a
Johnny Guitar (Nikolas Zorko) <nzorko@deadmattergame.com>
2016-03-31 16:42:45 -0600

    struct enhancements

--- gamemode/libraries/lib_luastructs.lua
+++ gamemode/libraries/lib_luastructs.lua
@@ -1,16 +1,72 @@
 --[[
 	Filename: sh_luastructs.lua
 	Notes: I'm too used to C++ and I hate having to constantly do type checking bullshit.
+	Now with metatables!
+--]]
+rain.struct = {}
+rain.structs = {} -- table containing all structs
+
+local rain_structmeta = {s_struct = true}
+
+--[[
+	Name: Is Struct
+	Category: Returns wether or not the table is a struct
+	Desc: Returns true if a table is a struct
 --]]
 
-rain.structs = {} -- table containing all structs
+function rain_structmeta:IsStruct()
+	return self.s_struct
+end
+
+--[[
+	Name: Get Unique Identifier
+	Category: Structs
+	Purpose: Gets the unique identifier for a struct
+--]]
+
+function rain_structmeta:GetUniqueIdentifier()
+	return s_uid or "_invalidstruct_"
+end
+
+--[[
+	Name: Compare
+	Category: Structs
+	Desc: Compares a struct to another, if they have the same unique identifier and therefore are the same type of struct return true
+--]]
+
+function rain_structmeta:Compare(tTestStruct)
+	if tTestStruct:IsStruct() then
+		if self:GetUniqueIdentifier() == tTestStruct:GetUniqueIdentifier() then
+			return true
+		end
+	end
+
+	return false
+end
+
+--[[
+	Name: Matches
+	Category: Structs
+	Desc: Gets wether or not the struct has the unique identifier in the argument
+--]]
+
+function rain_structmeta:Matches(sUniqueIdentifier)
+	if (sUniqueIdentifier) then
+		return self:GetUniqueIdentifier() == sUniqueIdentifier
+	end
+
+	return false
+end
+
+rain_structmeta.__index = rain_structmeta
 
 --[[
 	Function: RegisterStruct
 	Purpose: These are registered here as a generic struct, the purpose of the struct is to make it so that there is always a set default value.
 --]]
 
-function rain:RegisterStruct(sUniqueIdentifier, tStruct)
+function rain.struct:RegisterStruct(sUniqueIdentifier, tStruct)
+	tStruct.s_uid = sUniqueIdentifier
 	self.structs[sUniqueIdentifier] = tStruct
 end
 
@@ -19,8 +75,9 @@ end
 	Purpose: Gets a structured table from the master table.
 --]]
 
-function rain:GetStruct(sUniqueIdentifier)
+function rain.struct:GetStruct(sUniqueIdentifier)
 	local ret = {}
 	ret = table.Copy(self.structs[sUniqueIdentifier])
+	setmetatable(ret, rain_structmeta)
 	return ret
 end
