159f00a17b05fddadfc3a630f6c5b926214f9270
Johnny Guitar (Nikolas Zorko) <nzorko@deadmattergame.com>
2016-04-08 17:43:59 -0600

    basic chat implementation

--- gamemode/core/sh_chat.lua
+++ gamemode/core/sh_chat.lua
@@ -10,7 +10,23 @@ CHAT_PM = 2
 CHAT_PDA = 3
 CHAT_ADMIN = 4
 
-rain.struct:RegisterStruct("ChatStruct", {nRadius = 1024, sFont = "Arial", sIcon = "default", })
+TYPING_OOC = 0
+TYPING_IC = 1
+TYPING_ME = 2
+TYPING_IT = 3
+TYPING_YELLING = 4
+TYPING_WHISPERING = 5
+
+rain.chat.typingstrings = {}
+rain.chat.typingstrings[TYPING_OOC] = "typing"
+rain.chat.typingstrings[TYPING_IC] = "talking"
+rain.chat.typingstrings[TYPING_ME] = "performing"
+rain.chat.typingstrings[TYPING_IT] = "improvising"
+rain.chat.typingstrings[TYPING_YELLING] = "yelling"
+rain.chat.typingstrings[TYPING_WHISPERING] = "whispering"
+
+
+rain.struct:RegisterStruct("ChatStruct", {nRadius = 1024, sFont = "Arial", sIcon = "default", prefix = "//"})
 
 --[[
 	Name: Add
@@ -19,7 +35,11 @@ rain.struct:RegisterStruct("ChatStruct", {nRadius = 1024, sFont = "Arial", sIcon
 --]]
 
 function rain.chat.add(tChatStruct, fnOnChat, fnFormatText)
-
+	if tChatStruct:IsStruct() then
+		if tChatStruct:Matches("ChatStruct") then
+			rain.chatbuffer[tChatStruct.prefix] = {struct = tChatStruct, OnChat = fnOnChat, FormatText = fnFormatText}
+		end
+	end
 end
 
 --[[
@@ -48,11 +68,46 @@ end
 --[[
 	Name: Add In Radius
 	Category: Chat
-	Desc: Adds chat to all players in a specific radius, useful for player zones
+	Desc: Adds chat to all players in a specific radius, useful for player zones, CastToTarget means that the chat is muffled if there are barriers between the two people talking.
 --]]
 
-function rain.chat.addinradius(nRadius, enumType, sText)
+function rain.chat.addinradius(vPos, nRadius, enumType, sText, bCastToTarget)
+	for k, v in pairs(ents.FindInSphere(vPos, nRadius)) do
+		if v:IsPlayer() then
+			v:AddChat(enumType, sText)
+		end
+	end
+end
 
+--[[
+	Name: Get Typing Text
+	Category: Chat
+	Desc: returns the string used to display over a players head when they are typing.
+--]]
+
+function rain.chat.gettypingtext(enumTypingText)
+	if enumTypingText then
+		return rain.chat.typingstrings[enumTypingText]
+	else
+		return "typing"
+	end
+end
+
+--[[
+	Name: Player Say
+	Category: Chat
+	Desc: Called when a player sends out a chat message
+--]]
+
+function rain:PlayerSay(pSender, sText, bTeamChat)
+	if string.StartWith(sText, "/") then
+		-- do additional shit here
+	else
+		-- assume IC chat here
+		rain.chat.addinradius(pSender:GetPos(), 1024, 1, pSender:GetRPName()..": "..sText)
+	end
+
+	return "" -- always return nothing
 end
 
 local rainclient = FindMetaTable("Player")
@@ -63,7 +118,45 @@ local rainclient = FindMetaTable("Player")
 	Desc: Adds chat to a specific players chatbox
 --]]
 
-function rainclient:addchat(enumType, sText)
+if (SV) then
+	util.AddNetworkString("rain.chat.addchat")
+else
+	net.Receive("rain.chat.addchat", function()
+		LocalPlayer():AddChat(rain.net.ReadUByte(), net.ReadString())
+	end)
+end
+
+function rainclient:AddChat(enumType, sText)
+	local sText = sText or ""
+	local enumType = enumType or CHAT_IC
+
+	if (CL) then
+		chat.AddText(sText)
+	else
+		net.Start("rain.chat.addchat")
+		rain.net.WriteUByte(enumType)
+		net.WriteString(sText)
+		net.Send(self)
+	end
+end
+
+--[[
+	Name: Get Chat Info
+	Category: Chat->PlayerMeta
+	Desc: Gets a players chat info, this is wether they are typing/not typing, the type of chat they're performing, etc.
+--]]
+
+function rainclient:GetChatInfo()
+
+end
+
+--[[
+	Name: Set Chat Info
+	Category: Chat->PlayerMeta
+	Desc: Sets the players chat info, this is called from client to the server hence why the variables are strictly type checked.
+--]]
+
+function rainclient:SetChatInfo(bTyping, enumTypingText)
 
 end
 
--- gamemode/core/sh_playermeta.lua
+++ gamemode/core/sh_playermeta.lua
@@ -1,16 +1,24 @@
 
-	local rainclient = FindMetaTable("Player")
+local rainclient = FindMetaTable("Player")
 
-if (CL) then
+--[[
+	Name: Get RP Name
+	Category: PlayerMeta
+	Desc: Returns the players RP name, this is a detour function
+--]]
 
-	--[[
-		Name: 
-		Category: 
-		Desc: 
-	--]]
+function rainclient:GetRPName()
+	return self:GetVisibleRPName()
+end
 
-	function rainclient:GetVisibleRPName()
+--[[
+	Name: Get Visible RP Name
+	Category: PlayerMeta
+	Desc: Returns the players RP name by grabbing their character name, this can be modified for stuff like disguises later on.
+--]]
 
+function rainclient:GetVisibleRPName()
+	if self.character then
+		return self.character:GetName()
 	end
-	
 end
