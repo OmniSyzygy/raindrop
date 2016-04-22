54f3bcec486feeafc7a617243aeb2e866f5772bf
Johnny Guitar (Nikolas Zorko) <nzorko@deadmattergame.com>
2016-04-21 20:29:04 -0600

    chatbox update

--- gamemode/gui/cl_chatbox.lua
+++ gamemode/gui/cl_chatbox.lua
@@ -6,11 +6,23 @@ function PANEL:Init()
 	self.HTMLPanel:LoadHTMLComponent("chatbox")
 	self.HTMLPanel:SetPos(0, 22)
 	self.HTMLPanel:SetSize(650, 350)
-	for i = 1, 60 do
-		self.HTMLPanel:RunJavascript('chatbox.AddMessage("testman", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt augue ex, in hendrerit ligula maximus eget. Proin at orci lacus. Ut eget diam neque. Curabitur elementum fringilla purus in euismod. Etiam porttitor mi in venenatis lacinia. Nunc in felis efficitur, hendrerit dui eu, feugiat arcu. Curabitur pellentesque, velit in rhoncus ullamcorper, nisi erat accumsan lorem, viverra vehicula tortor sem ac dolor. Vestibulum vel tincidunt arcu, ac malesuada tortor. Duis et risus placerat, aliquam urna nec, ultrices turpis. Phasellus rutrum vehicula dapibus. ")')
+	for i = 1, 25 do
+		self.HTMLPanel:RunJavascript('chatbox.AddMessage("johnny guitar", "hello world! '..tostring(i)..'")')
 	end
 
 	self.chatopen = false
+
+	self.chatinput = vgui.Create("DTextEntry", self)
+	self.chatinput:SetPos(0, 275)
+	self.chatinput:SetSize(650, 24)
+	self.chatinput:RequestFocus()
+
+	self.chatinput.OnEnter = function()
+		self.HTMLPanel:RunJavascript('chatbox.AddMessage("johnny guitar", "'..string.JavascriptSafe(tostring(self.chatinput:GetValue()))..'")')
+
+		self.chatinput:SetText("")
+		self.chatinput:RequestFocus()
+	end
 end
 
 function PANEL:Paint()
